b9a1ae38a1347220777f63e39e6c57beaa33a20f
Lev_Gordon <lev.gordonsa@gmail.com>
2022-11-12 18:07:15 -0800

    navbar and styling for most of the document

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Link from 'next/link'
+import {AiOutlineShopping} from 'react-icons/ai'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='navbar-container'>
+        <p className='logo'>
+            <Link href='/'>LG HEADPHONE</Link>
+        </p>
+
+        <button type='button' className='cart-icon' onClick={''}>
+            <AiOutlineShopping />
+            <span className='cart-item-qty'>TOTAL QUANTITY</span>
+
+        </button>
+        
+    </div>
   )
 }
 
