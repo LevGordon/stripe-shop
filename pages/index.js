c1f8b536055ecf404c8c5a01afcaf383656c2396
Lev_Gordon <lev.gordonsa@gmail.com>
2022-11-12 16:35:07 -0800

    added HeroBanner, connected sanity API to Next and styled a little

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import { client } from '../lib/client'
+
 import {Product, FooterBanner, HeroBanner} from '../components'
 
-const Home = () => {
+const Home = ({products, bannerData}) => {
   return (
     <>
-     HeroBanner
+     <HeroBanner bannerData={bannerData.length && bannerData[0]}/>
 
      <div className='products-heading'>
       <h2>Best selling Products!</h2>
@@ -13,14 +15,26 @@ const Home = () => {
      </div> 
 
      <div className='products-container'>
-      {['Product 1', 'Product 2', 'Product 3'].map((product) => product)}
+      {products?.map((product) => product.name)}
      </div>
 
 
 
-     Footer
+     <FooterBanner />
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: {products, bannerData}
+  }
 }
 
 export default Home
