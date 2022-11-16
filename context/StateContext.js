ffc4e9ed5f8ed76ece288f840ec9ff1b674eeb5d
Lev_Gordon <lev.gordonsa@gmail.com>
2022-11-16 12:46:58 -0800

    cart functionality mostly done, bug preventing completion

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,6 +10,10 @@ const Context = createContext()
     const [totalQuantities, setTotalQuantities] = useState(0)
     const [qty, setQty] = useState(1)
 
+
+    let foundProduct;
+    let index;
+
     const onAdd = (product, quantity) => {
         const checkProductInCart = cartItems.find((item) => item._id === product._id)
 
@@ -32,6 +36,36 @@ const Context = createContext()
         }
 
         toast.success(`${qty} ${product.name} added to cart.`)
+    }
+
+    const onRemove = (product) => {
+        foundProduct = cartItems.find((item) => item._id === product._id)
+        const newCartItems = cartItems.filter((item) => item._id !== product._id)
+
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity)
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - foundProduct.quantity)
+        setCartItems(newCartItems)
+    }
+
+    const toggleCartItemQuantity = (id, value) => {
+        foundProduct = cartItems.find((item) => item._id === id)
+        index = cartItems.findIndex((product) => product._id === id)
+        const newCartItems = cartItems.filter((item) => item._id !== id)
+
+        if(value === 'inc') {
+            
+            setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }])
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1)
+        }   else if (value === 'dec') {
+
+                if( foundProduct.quantity > 1) {
+
+                    setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 }])
+                    setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+                    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1)
+                }
+            }
     }
 
 
@@ -58,6 +92,8 @@ const Context = createContext()
             incQty,
             decQty,
             onAdd,
+            toggleCartItemQuantity,
+            onRemove
         }}>
             {children}
         </Context.Provider>
