1948ef4951c07e8b6c38c8689078ee59f3ee0bad
Lev_Gordon <lev.gordonsa@gmail.com>
2022-11-16 11:52:00 -0800

    Cart visually done

--- context/StateContext.js
+++ context/StateContext.js
@@ -6,7 +6,7 @@ const Context = createContext()
  export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false)
     const [cartItems, setCartItems] = useState([])
-    const [totalPrice, setTotalPrice] = useState()
+    const [totalPrice, setTotalPrice] = useState(0)
     const [totalQuantities, setTotalQuantities] = useState(0)
     const [qty, setQty] = useState(1)
 
@@ -17,7 +17,7 @@ const Context = createContext()
             setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
             setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
 
-            const updatedCartItems = cartItems.map((cartItem) => {
+            const updatedCartItems = cartItems.map((cartProduct) => {
                 if(cartProduct._id === product._id) return {
                     ...cartProduct,
                     quantity: cartProduct.quantity + quantity
