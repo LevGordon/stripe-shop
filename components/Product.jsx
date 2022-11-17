ad3c35ce66d4f399941858d9462eb157a3160160
Lev_Gordon <lev.gordonsa@gmail.com>
2022-11-16 17:32:51 -0800

    stripe integration complete!

--- components/Product.jsx
+++ components/Product.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 import { urlFor } from '../lib/client'
 
-function Product({product: {image, name, slug, price}, key}) {
+function Product({product: {image, name, slug, price}}) {
   return (
     <div className=''>
       <Link href={`/product/${slug.current}`} >
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY)
 
 
 export default async function handler(req, res) {
@@ -15,20 +15,33 @@ export default async function handler(req, res) {
                     { shipping_rate: 'shr_1M4vg7DYdvYdetqW6f4OXkGj'},
                     { shipping_rate: 'shr_1M4vhODYdvYdetqW7T0gYm7Z'},
                 ],
-                line_items: [
-                  {
-                    price: '{{PRICE_ID}}',
-                    quantity: 1,
-                  },
-                ],
-                mode: 'payment',
+                line_items: req.body.map((item) => {
+                    const img = item.image[0].asset._ref
+                    const newImage = img.replace('image-', 'https://cdn.sanity.io/images/qis176ib/production/').replace('-webp', '.webp')
+
+                    return {
+                        price_data: {
+                            currency: 'usd',
+                            product_data: {
+                                name: item.name,
+                                images: [newImage]
+                            },
+                            unit_amount: item.price * 100,
+                        },
+                        adjustable_quantity: {
+                            enabled: true,
+                            minimum: 1,
+                        },
+                        quantity: item.quantity,
+                    }
+                }),
                 success_url: `${req.headers.origin}/?success=true`,
                 cancel_url: `${req.headers.origin}/?canceled=true`,
               }
 
             // Create Checkout Sessions from body params.
             const session = await stripe.checkout.sessions.create(params);
-            res.redirect(303, session.url);
+            res.status(200).json(session);
           } catch (err) {
             res.status(err.statusCode || 500).json(err.message);
           }
